7d895ae74a8a39025999f3f53854b79286354b2a
SUEP <noreply@shiep.edu.cn>
2022-10-01 09:12:57 +0800

    fix(ChromeOptions): add arguments in headless mode

--- main.py
+++ main.py
@@ -84,7 +84,10 @@ def main():
 
     if headless:
         options = Options()
-        options.headless = True
+        options.add_argument('--no-sandbox')
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
         confirm_time = 0
         driver = webdriver.Chrome(options=options)
     else:
